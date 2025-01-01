66fd51139ff872e1fc8dd026c0c7d0a634ce24bc
VedantSelmokar <vedant.selmokar@gmail.com>
2025-01-01 17:29:46 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -2,12 +2,12 @@ variable "aws_region" {
        description = "The AWS region to create things in." 
        default     = "ap-south-1" 
 }
-/*
+
 variable "key_name" { 
     description = " SSH keys to connect to ec2 instance" 
     default     =  "jenkins-server-key" 
 }
-*\
+
 variable "instance_type" { 
     description = "instance type for ec2" 
     default     =  "t2.micro" 
